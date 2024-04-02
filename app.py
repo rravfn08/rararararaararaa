ef0bebacddc6a2a913f879f1de9026ab77640547
rravfn08 <raraavtikafani@gmail.com>
2024-04-02 03:37:54 -0700

    commit pertama

---
+++ app.py
@@ -0,0 +1,12 @@
+print("## PROGRAM PYTHON MENGHITUNG LUAS SEGITIGA")
+print("==========================================")
+print()
+
+def hitungLuasSegitiga(a,t)
+return round (0.5 * a * t,2)
+
+alas = float (input("Input Alas Segitiga :"))
+tinggi = float (input("Input Tinggi Segitiga :"))
+
+print ('Luas Segitiga = ', hitungLuasSegitiga(alas,tinggi))
+print()
